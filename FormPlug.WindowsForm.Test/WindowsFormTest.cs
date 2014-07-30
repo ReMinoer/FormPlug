969225ab27ffb87e536abd86f69a88381d034ca3
ReMinoer <rmorillon@freneticexperience.com>
2014-07-31 00:48:49 +0200

    Add unit tests base to solution

---
+++ FormPlug.WindowsForm.Test/WindowsFormTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Forms;
+using FormPlug.Test;
+using FormPlug.WindowsForm.Plugs;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FormPlug.WindowsForm.Test
+{
+    [TestClass]
+    public class WindowsFormTest
+    {
+        [TestMethod]
+        public void PlugValueChanged()
+        {
+            FormPlugTestHelper.PlugValueChanged<NumericPlug<int>, NumericUpDown>();
+        }
+    }
+}
