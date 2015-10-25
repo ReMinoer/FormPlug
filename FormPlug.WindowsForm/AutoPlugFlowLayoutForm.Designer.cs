09d60a8b7b44fccf38eea6de7a32344652a1a07f
ReMinoer <rmorillon@freneticexperience.com>
2015-10-25 18:29:54 +0100

    Add AutoPlugFlowLayoutForm

---
+++ FormPlug.WindowsForm/AutoPlugFlowLayoutForm.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace FormPlug.WindowsForm
+{
+    partial class AutoPlugFlowLayoutForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.okButton = new System.Windows.Forms.Button();
+            this.parentPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // okButton
+            // 
+            this.okButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.okButton.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.okButton.Location = new System.Drawing.Point(197, 226);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(75, 23);
+            this.okButton.TabIndex = 1;
+            this.okButton.Text = "OK";
+            this.okButton.UseVisualStyleBackColor = true;
+            // 
+            // parentPanel
+            // 
+            this.parentPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.parentPanel.AutoScroll = true;
+            this.parentPanel.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.parentPanel.Location = new System.Drawing.Point(0, 0);
+            this.parentPanel.Name = "parentPanel";
+            this.parentPanel.Size = new System.Drawing.Size(284, 220);
+            this.parentPanel.TabIndex = 2;
+            // 
+            // AutoPlugFlowLayoutForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.parentPanel);
+            this.Controls.Add(this.okButton);
+            this.Name = "AutoPlugFlowLayoutForm";
+            this.Text = "AutoPlugFlowLayoutForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button okButton;
+        private System.Windows.Forms.FlowLayoutPanel parentPanel;
+
+    }
+}
---
+++ FormPlug.WindowsForm/AutoPlugFlowLayoutForm.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Forms;
+
+namespace FormPlug.WindowsForm
+{
+    public partial class AutoPlugFlowLayoutForm : Form
+    {
+        public AutoPlugFlowLayoutForm(object obj)
+        {
+            InitializeComponent();
+
+            var autoPlugPanel = new AutoPlugFlowLayoutPanel();
+            parentPanel.Controls.Add(autoPlugPanel);
+
+            autoPlugPanel.Connect(obj);
+        }
+    }
+}
